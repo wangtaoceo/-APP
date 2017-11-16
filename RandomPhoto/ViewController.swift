5e34cbf1e2e2b3c6d111bb44372f2114430f76d7
Olivia Su <oliviasu@sumengweide-MacBook-Air.local>
2017-11-16 18:50:56 +0800

    first version november 2017

--- RandomPhoto/ViewController.swift
+++ RandomPhoto/ViewController.swift
@@ -14,12 +14,17 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBOutlet weak var ImageView: UIImageView!
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonPressed(_ sender: Any) {
+        ImageView.image = UIImage(named: "image\(arc4random_uniform(6) + 1).jpg")
 
+    }
+    
 }
 
